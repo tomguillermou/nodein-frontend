e4c824155c842599ba6cb3e9eed87e38ca032e3a
tomguillermou <tomguillermou@gmail.com>
2019-10-17 00:07:47 +0200

    Add Users and Messages components to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
         HomeComponent,
-        RegisterComponent
+        RegisterComponent,
+        UsersComponent,
+        MessagesComponent
     ],
     imports: [
         BrowserModule,
