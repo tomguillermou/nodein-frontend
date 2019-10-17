a61677e9785c21196cf399aa0f848e318fbf4f05
tomguillermou <tomguillermou@gmail.com>
2019-10-17 10:21:57 +0200

    Add a "_components" folder inside "app" folder

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Post } from '../_models/post';
-import { HomeService } from '../_services/home.service';
+import { Post } from '../../_models/post';
+import { HomeService } from '../../_services/home.service';
 
 @Component({
     selector: 'app-home',
