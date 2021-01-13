9c352c0fa46e10d0064a10eb82c650ab01344968
Tom Guillermou <t.guillermou@citron.io>
2021-01-13 14:35:19 +0100

    🎨 Improve general CSS and countries component

--- src/app/shared/components/countries/countries.component.ts
+++ src/app/shared/components/countries/countries.component.ts
@@ -12,37 +12,21 @@ import { CountryService } from '../../../shared/services/country.service';
     styleUrls: ['./countries.component.css'],
 })
 export class CountriesComponent implements OnInit {
-    countries: Country[]; // using async pipe
-    country: Country;
+    countries: Country[];
+    selectedCountry: Country;
 
     constructor(private countryService: CountryService) {
         // Sort countries by hemisphere then population
         this.countryService.countries$.subscribe((countries) => {
-            this.countries = countries.sort((a, b) => {
-                let result: number;
-                const paths = ['continent', 'hemisphere', 'population'];
-
-                while (result == null && paths.length) {
-                    const path = paths.shift();
-
-                    const f1 = a[path];
-                    const f2 = b[path];
-
-                    if (f1 !== f2) {
-                        if (typeof f1 === 'number') {
-                            result = f2 - f1;
-                        } else if (typeof f1 === 'string') {
-                            result = f1.localeCompare(f2);
-                        }
-                    }
-                }
-
-                return result;
-            });
+            this.countries = this.sortCountries(countries, [
+                'continent',
+                'hemisphere',
+                'population',
+            ]);
         });
 
         this.countryService.country$.subscribe(
-            (country) => (this.country = country)
+            (country) => (this.selectedCountry = country)
         );
     }
 
@@ -50,5 +34,36 @@ export class CountriesComponent implements OnInit {
 
     public selectCountry(country: Country): void {
         this.countryService.emitCountry(country);
+    }
+
+    private sortCountries(
+        countries: Country[],
+        properties: string[]
+    ): Country[] {
+        const sortedCountries = countries.concat(); // shallow copy
+
+        sortedCountries.sort((a, b) => {
+            let result: number;
+            const paths = properties.concat(); // shallow copy
+
+            while (result == null && paths.length) {
+                const path = paths.shift();
+
+                const f1 = a[path];
+                const f2 = b[path];
+
+                if (f1 !== f2) {
+                    if (typeof f1 === 'number') {
+                        result = f2 - f1;
+                    } else if (typeof f1 === 'string') {
+                        result = f1.localeCompare(f2);
+                    }
+                }
+            }
+
+            return result;
+        });
+
+        return sortedCountries;
     }
 }
