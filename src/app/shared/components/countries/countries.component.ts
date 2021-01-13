38e88f80923ddc28af810732b61eb1b7b655b6c4
Tom Guillermou <t.guillermou@citron.io>
2021-01-13 11:50:40 +0100

    ⚡ Sort countries table over multiple properties

--- src/app/shared/components/countries/countries.component.ts
+++ src/app/shared/components/countries/countries.component.ts
@@ -2,32 +2,53 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-import { Country } from "../../../shared/models/db/Country";
+import { Country } from '../../../shared/models/db/Country';
 
-import { CountryService } from "../../../shared/services/country.service";
+import { CountryService } from '../../../shared/services/country.service';
 
 @Component({
-  selector: 'app-countries',
-  templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.css'],
+    selector: 'app-countries',
+    templateUrl: './countries.component.html',
+    styleUrls: ['./countries.component.css'],
 })
 export class CountriesComponent implements OnInit {
-
     countries: Country[]; // using async pipe
     country: Country;
 
-    constructor(
-        private countryService: CountryService,
-    ) {
-        this.countryService.countries$.subscribe(countries => this.countries = countries);
-        this.countryService.country$.subscribe(country => this.country = country);
+    constructor(private countryService: CountryService) {
+        // Sort countries by hemisphere then population
+        this.countryService.countries$.subscribe((countries) => {
+            this.countries = countries.sort((a, b) => {
+                let result: number;
+                const paths = ['continent', 'hemisphere', 'population'];
+
+                while (result == null && paths.length) {
+                    const path = paths.shift();
+
+                    const f1 = a[path];
+                    const f2 = b[path];
+
+                    if (f1 !== f2) {
+                        if (typeof f1 === 'number') {
+                            result = f2 - f1;
+                        } else if (typeof f1 === 'string') {
+                            result = f1.localeCompare(f2);
+                        }
+                    }
+                }
+
+                return result;
+            });
+        });
+
+        this.countryService.country$.subscribe(
+            (country) => (this.country = country)
+        );
     }
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
-    selectCountry(country: Country) {
+    public selectCountry(country: Country): void {
         this.countryService.emitCountry(country);
     }
-
 }
--- src/app/shared/models/db/Country.ts
+++ src/app/shared/models/db/Country.ts
@@ -2,6 +2,8 @@ export interface Country {
     id: number;
     name: string;
     flag: string;
+    hemisphere: 'Nord' | 'Sud';
+    continent: 'Europe' | 'Amerique' | 'Asie' | 'Afrique' | 'Oceanie';
     area: number;
     population: number;
 }
