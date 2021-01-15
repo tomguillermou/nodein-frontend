f8e483a28ebe387be26bad188b8e13c4c3d8c4aa
Tom Guillermou <t.guillermou@citron.io>
2021-01-15 10:24:24 +0100

    🔨 Place business logic inside service

--- src/app/shared/services/country.service.ts
+++ src/app/shared/services/country.service.ts
@@ -1,28 +1,59 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
-import { countries } from "../mocks/countries";
+import { countries } from '../mocks/countries';
 
 import { Country } from '../models/db/Country';
 
 @Injectable()
 export class CountryService {
+    private _countries$ = new BehaviorSubject<Country[]>(countries);
+    private _country$ = new Subject<Country>();
 
-  private _countries$ = new BehaviorSubject<Country[]>(countries);
-  private _country$ = new Subject<Country>();
+    constructor() {}
 
-  constructor() { }
+    public get countries$(): Observable<Country[]> {
+        return this._countries$.asObservable();
+    }
 
-  public get countries$() : Observable<Country[]> {
-    return this._countries$.asObservable();
-  }
-  
-  public get country$(): Observable<Country> {
-    return this._country$.asObservable();
-  }
+    public get country$(): Observable<Country> {
+        return this._country$.asObservable();
+    }
 
-  public emitCountry(country: Country): void {
-    this._country$.next(country);
-  }
+    public emitCountry(country: Country): void {
+        this._country$.next(country);
+    }
 
+    /**
+     * Sort list of countries by continent, hemisphere and population
+     * @param countries List of countries to sort
+     * @returns Sorted list of countries
+     */
+    public sortCountries(countries: Country[]): Country[] {
+        const sortedCountries = countries.concat();
+
+        sortedCountries.sort((a, b) => {
+            let result: number;
+            const paths = ['continent', 'hemisphere', 'population'];
+
+            while (result == null && paths.length) {
+                const path = paths.shift();
+
+                const f1 = a[path];
+                const f2 = b[path];
+
+                if (f1 !== f2) {
+                    if (typeof f1 === 'number') {
+                        result = f2 - f1;
+                    } else if (typeof f1 === 'string') {
+                        result = f1.localeCompare(f2);
+                    }
+                }
+            }
+
+            return result;
+        });
+
+        return sortedCountries;
+    }
 }
